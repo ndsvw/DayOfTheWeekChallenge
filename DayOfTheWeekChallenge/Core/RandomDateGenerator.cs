86bf6fa4d38ee58b81e7ac0fdb6498bd1bd465a0
Alex Lübeck <ndsvw@gmx.de>
2020-10-10 11:10:36 +0200

    Implemented visual calculator

---
+++ DayOfTheWeekChallenge/Core/RandomDateGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class RandomDateGenerator
+{
+    public static DateTime Generate(int pStartYear, int pEndYear)
+    {
+        var r = new Random();
+        var start = new DateTime(pStartYear, 1, 1);
+        var end = new DateTime(pEndYear, 12, 31);
+        int range = (end - start).Days;
+
+        var randomDate = start.AddDays(r.Next(range));
+
+        return randomDate;
+    }
+}
