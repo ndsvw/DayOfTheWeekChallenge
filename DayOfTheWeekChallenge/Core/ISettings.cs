d70af825f2ba6032b1ffd5b34d7393829b39d010
Alexander Lübeck <ndsvw@bitbucket.org>
2020-11-13 09:42:19 +0100

    Added setting to disable statistics

--- DayOfTheWeekChallenge/Core/ISettings.cs
+++ DayOfTheWeekChallenge/Core/ISettings.cs
@@ -7,5 +7,7 @@
         public LocalStorageSetting<bool> PreferNumbericAnswers { get; }
 
         public LocalStorageSetting<bool> PreferEfficientNumbers { get; }
+
+        public LocalStorageSetting<bool> IsStatisticsAreaVisible { get; }
     }
 }
--- DayOfTheWeekChallenge/Core/Settings.cs
+++ DayOfTheWeekChallenge/Core/Settings.cs
@@ -1,5 +1,4 @@
 ﻿using Blazored.LocalStorage;
-using Microsoft.AspNetCore.Components;
 
 namespace DayOfTheWeekChallenge.Core
 {
@@ -10,6 +9,7 @@ namespace DayOfTheWeekChallenge.Core
             IsSundayFirstDotw = new LocalStorageSetting<bool>(pLocalStorage, nameof(IsSundayFirstDotw), true);
             PreferNumbericAnswers = new LocalStorageSetting<bool>(pLocalStorage, nameof(PreferNumbericAnswers), false);
             PreferEfficientNumbers = new LocalStorageSetting<bool>(pLocalStorage, nameof(PreferEfficientNumbers), true);
+            IsStatisticsAreaVisible = new LocalStorageSetting<bool>(pLocalStorage, nameof(IsStatisticsAreaVisible), true);
         }
 
         public LocalStorageSetting<bool> IsSundayFirstDotw { get; }
@@ -17,5 +17,7 @@ namespace DayOfTheWeekChallenge.Core
         public LocalStorageSetting<bool> PreferNumbericAnswers { get; }
 
         public LocalStorageSetting<bool> PreferEfficientNumbers { get; }
+
+        public LocalStorageSetting<bool> IsStatisticsAreaVisible { get; }
     }
 }
